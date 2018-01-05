e5395ddd682578ceec79f34241d2f8b6f02b653e
abbydarda <darda.abby@gmail.com>
2018-01-05 20:03:53 +0700

    create oauth using passport

--- config/app.php
+++ config/app.php
@@ -173,11 +173,11 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
+        
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
